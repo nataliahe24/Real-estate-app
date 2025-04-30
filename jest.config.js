57d3fd33aa7860751ebc9b43bea77653dab7ef24
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-30 16:24:58 -0500

    feat: jest configuration

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+  testEnvironmentOptions: {
+    url: 'http://localhost/'
+  },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/'
+  }),
+  transform: {
+    '^.+\\.(ts|js|html|svg)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+        useESM: true
+      }
+    ]
+  }
+};
