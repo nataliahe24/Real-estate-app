614decb94808a395681f9f35baf239fae269ef1f
Natalia Henao <nataliahenaor24@gmail.com>
2025-04-30 00:50:12 -0500

    feat: add textarea atoms

---
+++ src/app/components/atoms/input/input.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements ControlValueAccessor {
+  @Input() label: string = '';
+  @Input() placeholder: string = '';
+  @Input() maxLength: number | null = null;
+  @Input() type: string = 'text';
+  
+  value: string = '';
+  disabled: boolean = false;
+  
+  onChange: any = () => {};
+  onTouch: any = () => {};
+  
+  writeValue(value: string): void {
+    this.value = value;
+  }
+  
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  
+  registerOnTouched(fn: any): void {
+    this.onTouch = fn;
+  }
+  
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+  
+  updateValue(value: string) {
+    this.value = value;
+    this.onChange(this.value);
+    this.onTouch();
+  }
+} 
