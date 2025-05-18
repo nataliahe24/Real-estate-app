3c4cf0ae96ee7a3e3904ead1472790d4c5b543e8
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-18 14:58:35 -0500

    feat: add property-form

---
+++ src/app/components/organisms/property-form/property-form.component.ts
@@ -0,0 +1,95 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Property } from '@app/core/models/property.model';
+
+@Component({
+  selector: 'app-property-form',
+  templateUrl: './property-form.component.html',
+  styleUrls: ['./property-form.component.scss']
+})
+export class PropertyFormComponent {
+  @Output() submitForm = new EventEmitter<Property>();
+
+  propertyForm: FormGroup;
+
+  readonly NAME_MAX_LENGTH = 100;
+  readonly NAME_MIN_LENGTH = 3;
+  readonly ADDRESS_MAX_LENGTH = 200;
+  readonly DESC_MAX_LENGTH = 500;
+  readonly PRICE_MIN = 0;
+  readonly ROOMS_MIN = 0;
+  readonly BATHROOMS_MIN = 0;
+
+  constructor(private fb: FormBuilder) {
+    this.propertyForm = this.fb.group({
+      name: ['', [
+        Validators.required,
+        Validators.maxLength(this.NAME_MAX_LENGTH)
+      ]],
+      address: ['', [
+        Validators.required,
+        Validators.maxLength(this.ADDRESS_MAX_LENGTH)
+      ]],
+      description: ['', [
+        Validators.required,
+        Validators.maxLength(this.DESC_MAX_LENGTH)
+      ]],
+      category: ['', [Validators.required]],
+      rooms: [0, [
+        Validators.required,
+        Validators.min(this.ROOMS_MIN)
+      ]],
+      bathrooms: [0, [
+        Validators.required,
+        Validators.min(this.BATHROOMS_MIN)
+      ]],
+      price: [0, [
+        Validators.required,
+        Validators.min(this.PRICE_MIN)
+      ]],
+      location: ['', [Validators.required]],
+      activePublicationDate: [new Date().toISOString().split('T')[0], [Validators.required]],
+      sellerId: [null, [Validators.required]]
+    });
+  }
+
+  get nameInfo() {
+    const control = this.propertyForm.get('name');
+    return {
+      currentLength: control?.value?.length || 0,
+      remainingChars: this.NAME_MAX_LENGTH - (control?.value?.length || 0),
+      isValid: control?.valid,
+      isDirty: control?.dirty
+    };
+  }
+
+  get addressInfo() {
+    const control = this.propertyForm.get('address');
+    return {
+      currentLength: control?.value?.length || 0,
+      remainingChars: this.ADDRESS_MAX_LENGTH - (control?.value?.length || 0),
+      isValid: control?.valid,
+      isDirty: control?.dirty
+    };
+  }
+
+  get descriptionInfo() {
+    const control = this.propertyForm.get('description');
+    return {
+      currentLength: control?.value?.length || 0,
+      remainingChars: this.DESC_MAX_LENGTH - (control?.value?.length || 0),
+      isValid: control?.valid,
+      isDirty: control?.dirty
+    };
+  }
+
+  onSubmit(): void {
+    if (this.propertyForm.valid) {
+      this.submitForm.emit(this.propertyForm.value);
+    }
+  }
+
+  onCancel(): void {
+    this.propertyForm.reset();
+  }
+} 
