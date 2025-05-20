759ac60e2e91aaae8c57ff1a34c7034dc13a54c6
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-19 19:49:41 -0500

    delete methods

--- src/app/core/services/categories/category.service.ts
+++ src/app/core/services/categories/category.service.ts
@@ -38,24 +38,6 @@ export class CategoryService {
       });
   }
    
-    getCategoryNames(orderAsc: boolean = true): Observable<string[]> {
-      console.log(`Requesting category names with orderAsc=${orderAsc}`);
-      
-      const params = new HttpParams().set('orderAsc', orderAsc.toString());
-      
-      const options = {
-        ...this.httpOptions,
-        params: params
-      };
-      console.log('Full request URL for category names:', `${this.apiUrl}list?${params.toString()}`);
-    
-      return this.http.get<any>(`${this.apiUrl}list`, options).pipe(
-        tap(data => {
-          console.log('Category names received:', data);
-        }),
-        catchError(this.handleError)
-      );
-    }
 
   getCategories(page: number = 0, size: number = 10, orderAsc: boolean = true): Observable<any> {
     console.log(`Requesting categories: page=${page}, size=${size}, orderAsc=${orderAsc}`);
