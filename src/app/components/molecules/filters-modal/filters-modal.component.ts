580fd31ec637f1e317ed9125bcd8ca55c3f3cd2d
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-22 20:48:16 -0500

    refactor: filter modal

--- src/app/components/molecules/filters-modal/filters-modal.component.ts
+++ src/app/components/molecules/filters-modal/filters-modal.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, takeUntil } from 'rxjs/operators';
 
 export interface FiltersModalConfig {
   roomsControl: FormControl;
@@ -15,12 +17,51 @@ export interface FiltersModalConfig {
   templateUrl: './filters-modal.component.html',
   styleUrls: ['./filters-modal.component.scss']
 })
-export class FiltersModalComponent {
+export class FiltersModalComponent  {
   @Input() open = false;
   @Input() config!: FiltersModalConfig;
   @Output() close = new EventEmitter<void>();
+  @Output() filtersChange = new EventEmitter<{
+    rooms: number | undefined;
+    bathrooms: number | undefined;
+    minPrice: number | undefined;
+    maxPrice: number | undefined;
+  }>();
+
+  private destroy$ = new Subject<void>();
 
   onClose(): void {
     this.close.emit();
+  }
+
+  resetFilters(): void {
+  
+    this.config.roomsControl.setValue('');
+    this.config.bathroomsControl.setValue('');
+    this.config.minPriceControl.setValue('');
+    this.config.maxPriceControl.setValue('');
+    
+    this.filtersChange.emit({
+      rooms: undefined,
+      bathrooms: undefined,
+      minPrice: undefined,
+      maxPrice: undefined
+    });
+  }
+
+  private emitFilters(): void {
+    const rooms = this.config.roomsControl.value ? Number(this.config.roomsControl.value) : undefined;
+    const bathrooms = this.config.bathroomsControl.value ? Number(this.config.bathroomsControl.value) : undefined;
+    const minPrice = this.config.minPriceControl.value ? Number(this.config.minPriceControl.value) : undefined;
+    const maxPrice = this.config.maxPriceControl.value ? Number(this.config.maxPriceControl.value) : undefined;
+
+    const filters = {
+      rooms,
+      bathrooms,
+      minPrice,
+      maxPrice
+    };
+
+    this.filtersChange.emit(filters);
   }
 } 
