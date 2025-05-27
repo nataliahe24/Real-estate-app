00c596f9af39e0845e47a42ae75df9ef496c9e31
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-27 15:41:09 -0500

    feat: username to template

--- src/app/components/templates/seller-user-layout/seller-user-layout.component.ts
+++ src/app/components/templates/seller-user-layout/seller-user-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { AuthService } from '@core/services/auth/auth.service';
 
 @Component({
   selector: 'app-seller-user-layout',
@@ -7,13 +8,17 @@ import { Component, Input } from '@angular/core';
 })
 export class SellerUserLayoutComponent {
   @Input() showSidebar: boolean = true;
+  
+  constructor(private readonly authService: AuthService) {}
+  
+  currentUser = this.authService.getCurrentUser();
 
   user = {
-    name: 'Vendedor',
+    name: this.currentUser?.name ?? '',
     avatar: '/assets/images/usuario.jpeg'
   };
   menuItems = [
     { icon: 'home', label: 'Publicaciones', route: '/publish'},
-    { icon: 'meeting_room', label: 'Citas', route: '/appointments'}
+    { icon: 'meeting_room', label: 'Visitas', route: '/visit'}
   ];
 }
