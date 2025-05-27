34cff8549c5e26110dfa257c7b4ce009fc0180e4
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-27 15:40:47 -0500

    feat: visit response

--- src/app/core/models/visit.model.ts
+++ src/app/core/models/visit.model.ts
@@ -1,6 +1,21 @@
-export interface VisitSchedule {
+export interface Visit {
   sellerId: number;
   propertyId: number;
   startDate: string;
   endDate: string;
+} 
+export interface VisitResponse {
+  content: Visit[];
+  page: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface VisitQueryParams {
+  startDate?: string;
+  endDate?: string;
+  location?: string;
+  page?: number;
+  size?: number;
 } 
