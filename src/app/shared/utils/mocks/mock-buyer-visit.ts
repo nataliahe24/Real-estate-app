f3ecd19b7ddcb9bcb80188145142314ca701aede
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-29 17:07:48 -0500

    feat: mock buyer-visit

---
+++ src/app/shared/utils/mocks/mock-buyer-visit.ts
@@ -0,0 +1,11 @@
+export const MOCK_BUYER_VISIT = {
+    scheduleId: 1,
+    buyerEmail: 'test@example.com'
+  };
+
+export const MOCK_RESPONSE = {
+    id: 1,
+    scheduleId: 1,
+    buyerEmail: 'test@example.com',
+    status: 'SCHEDULED'
+  };
