84b860ba6b45ee7314c1cec7b293b74744cfb80a
Natalia Henao <nataliahenaor24@gmail.com>
2025-05-29 13:49:42 -0500

    refactor: apiUrlBuyerVisit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8090/api/v1/',
   apiUrlProperties: 'http://localhost:8090/api/v1/properties/',
-  apiUrlBuyerVisit: 'http://localhost:8090/api/v1/buyer-visit/',
+  apiUrlBuyerVisit: 'http://localhost:8090/api/v1/buyer-visits/',
   apiUrlCategories: 'http://localhost:8090/api/v1/category/',
   apiUrlUsers: 'http://localhost:8030/api/v1/users/',
   apiUrlAuth: 'http://localhost:8030/api/v1/auth/',
