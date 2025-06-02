7b85c9fbbf76250aafd013158b8ed6cbd12cd551
Natalia Henao <nataliahenaor24@gmail.com>
2025-06-02 18:34:17 -0500

    refactor : service

--- src/app/core/services/properties/property.service.ts
+++ src/app/core/services/properties/property.service.ts
@@ -22,33 +22,6 @@ export class PropertyService {
     this.apiUrl = environment.apiUrlProperties;
   }
 
-  getProperties(filters: { [key: string]: any } = {}): Observable<PropertyResponse[]> {
-    let params = new HttpParams();
-
-    if (filters['category'] && filters['category'] !== '') {
-      params = params.append('category', filters['category']);
-    }
-    if (filters['location'] && filters['location'] !== '') {
-      params = params.append('location', filters['location']);
-    }
-    
-    params = params.append('page', '0');
-    params = params.append('size', '20');
-    params = params.append('orderAsc', 'true');
-    
-
-    return this.http.get<PaginatedPropertiesResponse>(`${environment.apiUrlProperties}list`, { params })
-      .pipe(
-        map((response: PaginatedPropertiesResponse) => response.content),
-        catchError(error => {
-          console.error('Error en la solicitud:', error);
-          if (error.error) {
-            console.error('Detalles del error:', error.error);
-          }
-          return throwError(() => error);
-        })
-      );
-  }
 
   createProperty(property: Property): Observable<Property> {
     return this.http.post<Property>(this.apiUrl, property).pipe(
@@ -63,12 +36,10 @@ export class PropertyService {
     let params = new HttpParams()
       .set('page', (filters.page || 0).toString())
       .set('size', (filters.size || 10).toString())
-      .set('orderAsc', (filters.orderAsc ?? true).toString());
-
-    const currentUserId = this.authService.getCurrentUser()?.id ?? null;
+      .set('orderAsc', (filters.orderAsc ?? true).toString())
+      .set('sortBy', filters.sortBy || 'price');
     
-    if (currentUserId) params = params.set('sellerId', currentUserId.toString());
-    if (filters.sortBy) params = params.set('sortBy', filters.sortBy);
+    if (filters.sellerId) params = params.set('sellerId', filters.sellerId.toString());
     if (filters.location) params = params.set('location', filters.location);
     if (filters.category) params = params.set('category', filters.category);
     if (filters.rooms) params = params.set('rooms', filters.rooms.toString());
